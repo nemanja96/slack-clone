561f4b64454b1477c95c51e9afad1484e0b28ee3
nemanja96 <nemanjaradivojevic1603@gmail.com>
2022-05-12 17:02:14 +0200

    Finished the Sidebar Section

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
-import Header from './Header';
+import styled from 'styled-components';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
       <div className="app">
-        <Routes>
-          <Route path="/" element={<Header />} />
-        </Routes>
+          <Header />
+          <AppBody>
+            <Sidebar />
+            {/* <Routes>
+              <Route path="/" exact />
+            </Routes> */}
+          </AppBody>
       </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`
